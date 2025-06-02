46d97a151957faeae3bf44658773af5da9d28e59
wisewinshin <dsds666a@naver.com>
2025-06-02 16:59:24 +0900

    버전 0.0.0에서 0.0.1로 업데이트

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0oq0sm5d13"
+    "revision": "0.08bgotg1j88"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
