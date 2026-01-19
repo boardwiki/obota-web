9bdf88030ba26bb0ff587f560c3a0f64a94f8e63
DESKTOP-7EG9OS2\jinji <jinji0326@naver.com>
2026-01-19 11:13:12 +0900

    updat e

---
+++ docs/assets/HomeView-Ck92LyFf.js
@@ -0,0 +1,1 @@
+import{d as o,o as e}from"./index-1EWdcCIn.js";const i=o({__name:"HomeView",setup(n){return e(()=>{window.location.href="https://boardwiki.kr"}),(t,r)=>null}});export{i as default};
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-console.log("https://boardwiki.kr")
-window.location.replace("https://boardwiki.kr")
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  window.location.href = 'https://boardwiki.kr'
+})
 </script>
 
-<template>
-</template>
+<template></template>
